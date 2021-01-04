7a15111d0792a7cd8d86d25520acae4c710884cf
shuhanmirza <shuhan.mirza@gmail.com>
2021-01-04 11:21:21 +0600

    update

--- main.py
+++ main.py
@@ -3,20 +3,7 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 import json
 import time
-
-'''
-import time
-from selenium import webdriver
-
-driver = webdriver.Chrome('/path/to/chromedriver')  # Optional argument, if not specified will search path.
-driver.get('http://www.google.com/');
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
-'''
+from datetime import datetime
 
 
 def login_to_oddo(driver, odoo_login_link, username, password):
@@ -37,14 +24,14 @@ def login_to_oddo(driver, odoo_login_link, username, password):
         break
 
 
-def give_attendance():
-    # Opening env.json file 
+def main():
+    # Opening env.json file
     f = open('env.json', )
 
-    # returns JSON object as a dictionary 
+    # returns JSON object as a dictionary
     credentials = json.load(f)
 
-    # Closing file 
+    # Closing file
     f.close()
 
     username = credentials['username']
@@ -57,64 +44,32 @@ def give_attendance():
 
     login_to_oddo(driver=driver, odoo_login_link=odoo_login_link, username=username, password=password)
 
-    time.sleep(1)
+    time.sleep(5)
 
+    give_attendance(driver=driver, odoo_attendance_link=odoo_attendance_link)
+
+
+def give_attendance(driver, odoo_attendance_link):
     driver.get(odoo_attendance_link)
 
     action = ActionChains(driver)
-    startElement = driver.find_element_by_tag_name('body')
-    action.move_to_element(startElement)
+    start_element = driver.find_element_by_tag_name('body')
+    action.move_to_element(start_element)
     action.perform()
 
     x_i = [20, 30]
     y_i = [20, 30]
 
     for mouse_x, mouse_y in zip(x_i, y_i):
-        action.move_by_offset(mouse_x, mouse_y);
+        action.move_by_offset(mouse_x, mouse_y)
         action.perform()
         action.click()
         print(mouse_x, mouse_y)
         time.sleep(5)
 
-    driver.save_screenshot('ss.png')
-    time.sleep(100)
-
-    # #then key here move to mobile version as that doesn't support javascript
-    # browser.get('https://m.facebook.com/'+target_username+'?v=timeline')
-    #
-    #
-    # # find last post (occurance of comment)
-    # as_el = browser.find_elements_by_tag_name('a')
-    # for a in as_el:
-    #     print(a.text)
-    #     if 'omment' in a.text.strip():
-    #         a.click()
-    #         break
-    # time.sleep(10)
-    #
-    # # it will go for 1000 comments
-    # i = comment_count
-    # while i > 0:
-    #     # do actual comment
-    #     ins = browser.find_element_by_id('composerInput')
-    #
-    #     cmnt = getRandomQuote()
-    #
-    #     ins.send_keys(cmnt)
-    #     # submit input
-    #
-    #     ins = browser.find_elements_by_tag_name('input')
-    #     for x in ins:
-    #         if 'omment' in x.get_attribute('value'):
-    #             x.click()
-    #             break
-    #
-    #     #random waiting
-    #     r = random.randrange(2,5)
-    #     time.sleep(r)
-    #
-    #     i = i - 1
+    driver.save_screenshot('screenshots/' + str(datetime.now()) + '.png')
+    print('Screen Shot Done')
 
 
 if __name__ == "__main__":
-    give_attendance()
+    main()
